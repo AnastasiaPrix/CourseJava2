e450c658034595889e412de45f4dd9d3f660723c
AnastasiaPrix <Ohcheeph1234>
2021-02-09 02:07:41 +0300

    Готовое первое ДЗ + задание с перечислениями

---
+++ src/Lesson1/enums/enumMain.java
@@ -0,0 +1,23 @@
+package Lesson1.enums;
+
+public class enumMain {
+    public static void main(String[] args) {
+        System.out.println("Осталось работать "+ getWorkingHours(DayOfWeek.Sunday)+" часов");
+    }
+
+    public static int getWorkingHours(DayOfWeek dayOfWeek){
+        int result = 0;
+        if ( dayOfWeek.getWorkingHours() == 0) {
+            System.out.println("Сегодня выходной день!");
+        }
+        else {
+            for (DayOfWeek d : DayOfWeek.values()) {
+                if (d.getNumberOfDay() >= dayOfWeek.getNumberOfDay()) {
+                    result = result + d.getWorkingHours();
+                }
+
+            }
+        }
+        return result;
+    }
+}
